a4679be2bb8959639bc3c951b125fb07d64a8e8a
Robert Sanders <robert@vitrue.com>
2008-09-03 04:57:03 -0400

    better word selection algorithm pick longest word if all have same frequency

--- epcp/eightify.rb
+++ epcp/eightify.rb
@@ -18,20 +18,29 @@ class Eightify
   end
   
   def commons(list)
-    list = list.map {|filename|  filename.gsub(/\.[^.]+$/, '') }
+    dp "commons incoming list is #{list}"
+    list = list.map {|filename|  filename.gsub(/\.[^.]+$/, '').downcase }
     
     string = list.join(" ")
-    counts = wordcount string.split
+    counts = wordcount (string.split(/[^\w]/).map {|word| word.chomp})
     pairs = []
-    counts.each { |word,count| pairs.push [word,count] if count > 1 }
-    counts.sort {|pair1, pair2| pair2[1] <=> pair1[1]}
+    counts.each { |word,count| pairs.push [word,count] if word != '' and count > 1 }
+    dp "commons unsorted is #{counts}"
+    counts.sort {|pair1, pair2| 
+      results = [pair2[1] <=> pair1[1], pair2[0].length <=> pair1[0].length]
+      results = results.delete_if {|x| x == 0}
+      results.length > 0 ? results[0] : 0
+    }
   end
   
   def wordcount(words)
     counts = {}
     words = words.sort.map { |word| 
-      counts[word] ||= 0
-      counts[word] += 1
+      word = word.chomp
+      if word.chomp != '' and not word.downcase.match(/^(the|a|an)$/)
+        counts[word] ||= 0
+        counts[word] += 1
+      end
     }
     dp "counts is #{counts}"
     counts
@@ -52,8 +61,10 @@ class Eightify
   end
   
   def getbase(list, maxsize=4)
-    commons = commons list
-    words = commons.map {|pair| pair[0] }
+    commonwords = commons list
+    dp "commonwords is #{commonwords}"
+    words = commonwords.map {|pair| pair[0] }
+    dp "base word is #{words[0]} next is #{words[1]}, reducing to #{maxsize} chars"
     word = reduce words[0], maxsize
     word
   end
@@ -83,7 +94,7 @@ class Eightify
 
   def process(list)
 
-    nums = getnums(list).map { |num| num.sub(/^0/, '') }
+    nums = getnums(list).map { |num| num.to_s.sub(/^0/, '') }
     dp "nums is #{nums}"
     
     numsize = nums.map {|num| num.length}.max
